c0c500c69521b70fb7b207ab497e0284231e00f7
salil-jt <saliljavatrainings@gmail.com>
2022-08-30 07:57:41 +0530

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+	
+public static void main(String[] args) {
+	
+	byte a = 126;
+	byte b = 126;
+	byte c = (byte)(a + b);
+	System.out.println(c);
+	
+	
+}
+
+}
+
+
+This is first project
